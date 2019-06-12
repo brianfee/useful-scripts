2f1f75e1263b503c8a502eb369aac7c066bcca11
Brian <brianfee7@gmail.com>
2019-06-12 11:41:08 -0600

    Added password switch

--- pdf-protect/pdf_protect.sh
+++ pdf-protect/pdf_protect.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# User switches
+while getopts p: arg; do
+	case "${arg}" in
+		p) password=${OPTARG};;
+	esac
+done
+shift "$(($OPTIND - 1))"
+
 if [ -f "$@" ]; then
 	of="${@/.pdf/\ \(Protected\).pdf}"
-	read -s -p "Password:" password
+
+	if [ -z "$password" ]; then
+		read -s -p "Password:" password
+	fi
+
 	pdftk "$@" output "$of" user_pw "$password"
 	echo
 	echo "Completed. Created File: $of"
