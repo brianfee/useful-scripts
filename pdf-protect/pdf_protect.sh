34e9a61fda7e51b6325306029c242f8ea43c1a6d
Brian <brianfee7@gmail.com>
2019-06-12 12:06:02 -0600

    Added verbosity switch

--- pdf-protect/pdf_protect.sh
+++ pdf-protect/pdf_protect.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
 
+# Default variables
+verbosity=0
+
 # User switches
-while getopts p: arg; do
+while getopts p:v arg; do
 	case "${arg}" in
 		p) password=${OPTARG};;
+		v) ((verbosity++));;
 	esac
 done
 shift "$(($OPTIND - 1))"
 
-if [ -f "$@" ]; then
+
+if [[ -f "$@" ]]; then
 	of="${@/.pdf/\ \(Protected\).pdf}"
 
-	if [ -z "$password" ]; then
+	if [[ -z "$password" ]]; then
 		read -s -p "Password:" password
+		echo
+	fi
+
+	if [[ "$verbosity" -gt 1 ]]; then
+		echo "Input File: $@"
+		echo "Output File: $of"
+		echo "Password: $password"
+		echo "Verbosity: $verbosity"
+		echo
 	fi
 
 	pdftk "$@" output "$of" user_pw "$password"
-	echo
-	echo "Completed. Created File: $of"
+
+	if ! [[ -f "$of" ]]; then
+		echo "Error during file creation."
+		exit
+
+	elif [[ "$verbosity" -ge 1 ]]; then
+		echo "Completed. Created File: $of"
+	fi
 
 else
 	echo File does not exist.
