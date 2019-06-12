da77a10a249b89703b792ecb947d1b928f7fcac3
Brian <brianfee7@gmail.com>
2019-06-12 10:09:14 -0600

    Added verbosity switch

--- lscsv/lscsv.sh
+++ lscsv/lscsv.sh
@@ -3,11 +3,13 @@
 # Set default variables
 depth=1
 path=.
+verbosity=0
 
 # User switches
-while getopts d: arg; do
+while getopts d:v arg; do
 	case "${arg}" in
 		d) depth=${OPTARG};;
+		v) ((verbosity++));;
 	esac
 done
 shift "$(($OPTIND - 1))"
@@ -16,6 +18,26 @@ shift "$(($OPTIND - 1))"
 # If path exists, override default
 if ! [[ -z "$@" ]] && ([[ -f "$@" ]] || [[ -d "$@/" ]]); then
 	path=$@
+
+elif [[ -z "$@" ]]; then
+	if [[ "$verbosity" -ge 1 ]]; then
+		echo "No path specified. Using current directory..."
+	fi
+
+else
+	echo "Invalid Path..."
+	exit
+fi
+
+if [[ "$verbosity" -gt 1 ]]; then
+	echo "Path:" $path
+	echo "Depth:" $depth
+	echo "Verbosity:" $verbosity
+	echo 
+fi
+
+if [[ "$verbosity" -ge 1 ]]; then
+	find $path -maxdepth $depth -ls
 fi
 
 find $path -maxdepth $depth -ls | python -c "
